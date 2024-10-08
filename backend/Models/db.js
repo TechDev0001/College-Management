2a713423811d5785f13e59fc18a3509e6fdb021b
shubham <shubhamyadav203920@gmail.com>
2024-10-08 15:50:51 +0530

    Error fixing..

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 const mongo_url = process.env.MONGO_URL;
 console.log("mongoUrl:",mongo_url)
-mongoose.connect(mongo_url,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-   
-})
+mongoose.connect(mongo_url)
 
 
     .then(() => {
